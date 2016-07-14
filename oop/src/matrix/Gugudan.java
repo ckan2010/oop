81d7676b4e2ab4fac4583a467c0f15b9185545df
ckan <ckan@hb2008>
2016-07-14 18:24:14 +0900

    2016-07-14 구구단 수정

--- oop/src/matrix/Gugudan.java
+++ oop/src/matrix/Gugudan.java
@@ -8,7 +8,6 @@ package matrix;
  */
 public class Gugudan {
 	public static void main(String[] args) {
-		String s = "";
 		for (int i = 2; i < 10; i=i+4) {
 			for (int j = 1; j < 10; j++) {
 				System.out.print(i+"*"+j+"="+i*j+"\t");
